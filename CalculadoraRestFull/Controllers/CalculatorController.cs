a7538c1f26fb0cd1b612cfc61beb9281106d3b5d
Rhuda Campos <rhudaocampos@gmail.com>
2023-05-22 08:48:47 -0300

    feat: add math operatoins on controller

---
+++ CalculadoraRestFull/Controllers/CalculatorController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraRestFull.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        //ToDo: Passar metodos para serviços
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(decimal firstNumber, decimal secondNumber)
+            => Ok(firstNumber + secondNumber);
+
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtraction(decimal firstNumber, decimal secondNumber)
+            => Ok(firstNumber - secondNumber);
+
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(decimal firstNumber, decimal secondNumber)
+            => Ok(firstNumber * secondNumber);
+
+        [HttpGet("divided-by/{firstNumber}/{secondNumber}")]
+        public IActionResult DividedBy(decimal firstNumber, decimal secondNumber)
+            => Ok(firstNumber / secondNumber);
+
+        [HttpGet("mean")]
+        public IActionResult Mean([FromQuery] decimal[] listNumber)
+            => Ok(listNumber.Average());
+
+        [HttpGet("square-root/{firstNumber}/{secondNumber}")]
+        public IActionResult SquareRoot(decimal number)
+            => Ok(Math.Sqrt(decimal.ToDouble(number)));
+
+            
+    }
+}
